b6b4f5aab9757a2511195db9230ba149297a123e
Nat Welch <nat@natwelch.com>
2013-04-10 00:50:19 -0700

    basics.

--- app/controllers.rb
+++ app/controllers.rb
@@ -4,6 +4,17 @@ MTV::App.controllers  do
   end
 
   post :index do
+    require 'fileutils'
+    require 'tempfile'
+
+    p params
+
+    datafile = params[:data]
+    Tempfile.new("music") do |file|
+      file.write(datafile[:tempfile].read)
+      logger.push "wrote to #{file.inspect}"
+    end
+
 
     redirect :index
   end
