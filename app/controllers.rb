62d56647e7947e2c64fb09d0bcc82b04e9a6868a
Nat Welch <nat@natwelch.com>
2013-04-13 01:47:30 -0700

    controllers

--- app/controllers.rb
+++ app/controllers.rb
@@ -17,14 +17,16 @@ MTV::App.controllers  do
     # Resize and move image
     image = Tempfile.new(['', params[:image][:filename]]).path
     img = Magick::Image::read(params[:image][:tempfile].path).first
-    img.resize_to_fill!(1280, 720)
+    #img.resize_to_fill!(1280, 720)
     img.write image
 
     # Combine image and music and write to a temp file.
     # More details: http://ffmpeg.org/trac/ffmpeg/wiki/EncodeforYouTube
     dest = Tempfile.new(['final', '.mkv']).path
     #cmd = "avconv -y -loop 1 -r 2 -i \"#{image}\" -i \"#{music}\" -c:v libx264 -preset medium -tune stillimage -aspect 16:9 -crf 18 -c:a libvorbis -q:a 5 -pix_fmt yuv420p \"#{dest}\""
-    cmd = "avconv -y -loop 1 -r 2 -i \"#{image}\" -i \"#{music}\" -aspect 16:9 -crf 18 -c:a libvorbis -q:a 5 -pix_fmt yuv420p \"#{dest}\""
+    #cmd = "avconv -y -loop 1 -r 2 -i \"#{image}\" -i \"#{music}\" -aspect 16:9 -crf 18 -c:a libvorbis -q:a 5 -pix_fmt yuv420p \"#{dest}\""
+    #cmd = "avconv -y -loop 1 -r 2 -i \"#{image}\" -i \"#{music}\" -crf 18 -c:a copy -shortest -pix_fmt yuv420p output.mkv"
+    cmd = "avconv -y -loop 1 -r 2 -i \"#{image}\" -i \"#{music}\" -crf 18 -c:a libvorbis -q:a 5 -shortest -pix_fmt yuv420p \"#{dest}\""
     puts cmd
     Kernel.system cmd
 
