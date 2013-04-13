bce528e108b4e95422c47347e9f7e2d87f74772f
Nat Welch <nat@natwelch.com>
2013-04-13 00:51:37 -0700

    fuck. Work.

--- app/controllers.rb
+++ app/controllers.rb
@@ -10,18 +10,24 @@ MTV::App.controllers  do
   post :index do
     require 'fileutils'
 
-    dest = Tempfile.new(['final', '.mkv']).path
-    image = Tempfile.new(['', params[:image][:filename]]).path
+    # Move music file
     music = Tempfile.new(['', params[:music][:filename]]).path
-
-    FileUtils.mv(params[:image][:tempfile].path, image)
     FileUtils.mv(params[:music][:tempfile].path, music)
 
+    # Resize and move image
+    image = Tempfile.new(['', params[:image][:filename]]).path
+    img = Magick::Image::read(params[:image][:tempfile].path).first
+    img.resize_to_fill!(1280, 720)
+    img.write image
+
+    # Combine image and music and write to a temp file.
     # More details: http://ffmpeg.org/trac/ffmpeg/wiki/EncodeforYouTube
+    dest = Tempfile.new(['final', '.mkv']).path
     cmd = "avconv -y -i \"#{image}\" -i \"#{music}\" -c:v libx264 -preset medium -tune stillimage -aspect 16:9 -crf 18 -c:a libvorbis -q:a 5 -pix_fmt yuv420p \"#{dest}\""
     puts cmd
     Kernel.system cmd
 
+    # Move final output to a tmp/ to serve
     serv = "tmp/#{Time.now.to_i}.mkv"
     FileUtils.mv(dest, serv)
 
